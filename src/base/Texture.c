ca42b347bfe9c9775c8af869527ffeac7a32d7ec
Fábio Pichler <fabiopichler1992@gmail.com>
2022-06-01 13:48:48 -0300

    updated

--- src/base/Texture.c
+++ src/base/Texture.c
@@ -123,7 +123,7 @@ bool Texture_MakeText(Texture * const self)
 
     SDL_Color color = { self->textColor.r, self->textColor.g, self->textColor.b, self->textColor.a };
     SDL_Surface *text = TTF_RenderUTF8_Blended(self->font, self->text, color);
-    SDL_Surface *surface = SDL_CreateRGBSurface(0, text->w, text->h, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+    SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, text->w, text->h, 32, SDL_PIXELFORMAT_BGRA32);
 
     SDL_BlitSurface(text, NULL, surface, NULL);
     SDL_FreeSurface(text);
